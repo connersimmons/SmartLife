f82e7c3255c5b209a7c33a618c3f522f7ca51906
Conner Simmons <cwsimmons149@gmail.com>
2016-04-12 14:49:26 -0400

    Initial Commit

---
+++ SmartLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartLife
+//
+//  Created by Conner Simmons on 4/12/16.
+//  Copyright © 2016 Conner Simmons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
